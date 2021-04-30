35d6626bb34726a08da10d34a08c28ca1e8bf1ab
japocoinyenhoang <japocoin@gmail.com>
2021-04-30 16:51:09 +0200

    protocols info added

--- conceptos2/Protocolos.playground/Contents.swift
+++ conceptos2/Protocolos.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+//Protocolos
+
+protocol PersonasProtocol {
+    var nombre: String { get set }
+    var edad: Int { get set }
+    
+    func nombreCompletar() ->String
+}
+
+struct Alumno: PersonasProtocol {
+    var nombre: String
+    var edad: Int
+    var curso: String
+    
+    func nombreCompletar() -> String {
+        return nombre
+    }
+}
+
+struct Profesor: PersonasProtocol{
+    var nombre: String
+    var edad: Int
+    var materiaImparte: String
+    var cursoImparte: String
+    
+    func nombreCompletar() -> String {
+        return nombre
+    }
+}
+
+let miAlummno = Alumno(nombre: "Peter", edad: 33, curso: "Aplicaciones")
+
+let miProfesor = Profesor(nombre: "Juan", edad: 24, materiaImparte: "IOS", cursoImparte: "Aplicaciones")
